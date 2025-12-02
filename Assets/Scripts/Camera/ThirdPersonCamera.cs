ba209844f8e55d2bd21380d2ad6bb8f71c8797cc
Akop <akopasatryan666@gmail.com>
2025-12-03 00:26:36 +0300

    v0.0.4
    add crosshair;
    new camera diraction;

--- Assets/Scripts/Camera/ThirdPersonCamera.cs
+++ Assets/Scripts/Camera/ThirdPersonCamera.cs
@@ -10,6 +10,14 @@ public class ThirdPersonCamera : MonoBehaviour
     public Transform playerObj;
     public Rigidbody rb;
 
+    public CameraStyle currentcamera;
+    public Transform CombatLook;
+    public enum CameraStyle
+    {
+        Basic,
+        Combat
+    }
+
     public float rotationSpeed;
 
     private void Start()
@@ -23,13 +31,24 @@ public class ThirdPersonCamera : MonoBehaviour
         Vector3 viewDir = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
         orientation.forward = viewDir.normalized;
 
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        Vector3 inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
-
-        if (inputDir != Vector3.zero)
+        if (currentcamera == CameraStyle.Basic)
         {
-            playerObj.forward = Vector3.Slerp(playerObj.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
+            float horizontalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
+            Vector3 inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+            if (inputDir != Vector3.zero)
+            {
+                playerObj.forward = Vector3.Slerp(playerObj.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
+            }
+        }
+        
+        else if (currentcamera == CameraStyle.Combat)
+        {
+            Vector3 dirToCombat = CombatLook.position - new Vector3(transform.position.x, CombatLook.position.y, transform.position.z);
+            orientation.forward = dirToCombat.normalized;
+
+            playerObj.forward = dirToCombat.normalized; 
         }
     }
 }
